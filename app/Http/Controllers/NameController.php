1144b8e6543948b6079e070a7daa66915cbe6e83
kushtra24 <kushtra24@gmail.com>
2020-09-18 21:55:04 +0200

    krijuar nje migracion per tablen names, modeli Name dhe kontrolleri NameController. shkruar metoren index dhe store

---
+++ app/Http/Controllers/NameController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Category;
+use App\Helpers\Utils;
+use App\Models\Name;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+
+class NameController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index()
+    {
+        $name = Name::get(); // select * from names;
+
+        return response()->json($name, 200);
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request)
+    {
+
+        $name = new Name;
+
+        $name->name = $request['name'];
+        $name->origin = $request['origin'];
+        $name->description = $request['description'];
+        $name->gender = $request['gender'];
+        $name->meaning = $request['meaning'];
+
+        $name->save();
+
+//        $name = Name::create($request->all());
+
+        return response()->json($name, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('emarat', 'NameController@index');
+Route::post('emarat', 'NameController@store');
+Route::put('emarat/{id}', 'NameController@update');
+Route::delete('emarat/{id}', 'NameController@destroy');
